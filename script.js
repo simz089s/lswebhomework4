a3d46c65127935d9e136aab2fe942eb618913d7b
simz089s <simz089s@users.noreply.github.com>
2017-05-31 03:28:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.body.style.backgroundColor = "red";
+
+document.body.getElementsByTagName("h1")[0].style.backgroundColor = "blue";
+
+document.body.style.fontFamily = "sans-serif";
+
+document.getElementById("nickname").innerHTML += "SimZ";
+
+document.getElementById("favorites").innerHTML += "Blue, 12, EDM";
+
+document.getElementById("hometown").innerHTML += "Montréal";
+
+var list = document.body.getElementsByTagName("li");
+for (var i = 0; i < list.length; i++) {
+	list[i].className = "listitem";
+}
+
+document.head.appendChild(document.createElement("style"));
+document.head.getElementsByTagName("style")[0].innerHTML += ".listitem { color: #7F0000; }";
+
+document.body.appendChild(document.createElement("img"));
+document.body.getElementsByTagName("img")[0].src = "pic.jpg" ;
